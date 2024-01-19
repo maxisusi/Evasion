a238c170b12d53f995843710e10d5b555d41f348
maxisusi <maxbalej@proton.me>
2024-01-19 14:02:51 +0100

    Adding String() to render each statements as human readable data

--- ast/ast.go
+++ ast/ast.go
@@ -1,8 +1,12 @@
 package ast
 
-import "evasion/token"
+import (
+	"bytes"
+	"evasion/token"
+)
 
-// Root node of AST
+/* ### PROGRAM ### */
+
 type Program struct {
 	Statements []Statement
 }
@@ -15,30 +19,71 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
-// All nodes in the AST must implement the Node interface
-type Node interface {
-	TokenLiteral() string
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
 }
 
+/* ### NODE ### */
+
+type Node interface {
+	TokenLiteral() string
+	String() string
+}
+
+/* ### STATEMENT ### */
 type Statement interface {
 	Node
 	statementNode()
 }
+
+/* ### EXPRESSION ### */
+
+type ExpressionStatement struct {
+	Token      token.Token // the first token of the expression
+	Expression Expression
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
 
 type Expression interface {
 	Node
 	expressionNode()
 }
 
+/* ### RETURN STATEMENT ### */
+
 type ReturnStatement struct {
 	Token       token.Token
 	ReturnValue Expression
 }
 
-func (rs *ReturnStatement) statementNode() {}
-func (rs *ReturnStatement) TokenLiteral() string {
-	return rs.Token.Literal
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+	out.WriteString(";")
+
+	return out.String()
 }
+
+func (rs *ReturnStatement) statementNode()       {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
 
 type LetStatement struct {
 	Token token.Token
@@ -46,23 +91,32 @@ type LetStatement struct {
 	Value Expression
 }
 
-// Satisfies the Statement interface
-func (ls *LetStatement) statementNode() {}
+/* ### LET STATEMENT ### */
 
-// Satisfies the Node interface
-func (ls *LetStatement) TokenLiteral() string {
-	return ls.Token.Literal
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+	out.WriteString(";")
+
+	return out.String()
 }
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+/* ### IDENTIFIER ### */
 
 type Identifier struct {
 	Token token.Token
 	Value string
 }
 
-// Satisfies the Expression interface
-func (i *Identifier) expressionNode() {}
-
-// Satisfies the Node interface
-func (i *Identifier) TokenLiteral() string {
-	return i.Token.Literal
-}
+func (i *Identifier) String() string       { return i.Value }
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
