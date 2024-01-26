aceb6ccda914b7c2d5a52e1b484d49fcd9dd4143
maxisusi <maxbalej@proton.me>
2024-01-26 16:52:49 +0100

    Migrating the repl to parse and display the tokens

--- repl/repl.go
+++ repl/repl.go
@@ -3,6 +3,7 @@ package repl
 import (
 	"bufio"
 	"evasion/lexer"
+	"evasion/parser"
 	"evasion/token"
 	"fmt"
 	"io"
@@ -12,6 +13,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	io.WriteString(out, EVASION_LOGO)
 
 	for {
 		fmt.Fprint(out, PROMPT)
@@ -22,9 +24,35 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
+		}
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
 
 		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
 			fmt.Fprintf(out, "%+v\n", tok)
 		}
+	}
+}
+
+const EVASION_LOGO = `
+                           .__               
+  _______  _______    _____|__| ____   ____  
+_/ __ \  \/ /\__  \  /  ___/  |/  _ \ /    \ 
+\  ___/\   /  / __ \_\___ \|  (  <_> )   |  \
+ \___  >\_/  (____  /____  >__|\____/|___|  /
+     \/           \/     \/               \/ 
+
+`
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
