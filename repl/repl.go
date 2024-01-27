4c45db3e717d3bb5171bcefc25363b1017d5aa4a
maxisusi <maxbalej@proton.me>
2024-01-27 11:21:31 +0100

    Evalutating Integers

--- repl/repl.go
+++ repl/repl.go
@@ -2,6 +2,7 @@ package repl
 
 import (
 	"bufio"
+	"evasion/evaluator"
 	"evasion/lexer"
 	"evasion/parser"
 	"evasion/token"
@@ -31,8 +32,11 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 
 		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
 			fmt.Fprintf(out, "%+v\n", tok)
